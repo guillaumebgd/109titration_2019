a9f2066bf71bde4d93a7c5f13e85baf0bc01e360
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-03-30 18:41:04 +0200

    [First push] -> file management handled

---
+++ error_handling/usage.py
@@ -0,0 +1,15 @@
+# -*- coding: Utf-8 -*
+##
+## EPITECH PROJECT, 2019
+## 109titration_2019
+## File description:
+## usage.py
+##
+
+def usage():
+    usage = """USAGE
+\t./109titration file
+
+DESCRIPTION
+\tfile\ta csv file containing "vol;ph" lines"""
+    print(usage)
