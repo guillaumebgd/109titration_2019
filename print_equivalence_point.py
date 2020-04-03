140b9b6f5817abda0166d6e118bbc0d82eca1fcf
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-04-03 13:01:03 +0200

    Update Equivalence point

--- print_equivalence_point.py
+++ print_equivalence_point.py
@@ -6,15 +6,15 @@
 ## print_equivalence_point.py
 ##
 
-def print_equivalence_point(deriv: list, stock: list) :
+def print_equivalence_point(stock: list) :
 
-    eq_pt = stock[1][0]
+    eq_pt = float(stock[0][0])
     pos_eq = 0
 
-    for i in range(1, len(stock) - 1) :
-        if deriv[i - 1] > eq_pt :
-            eq_pt = stock[i][0]
-            pos_eq = i - 1
+    for i in range(0, len(stock)) :
+        if float(stock[i][1]) > eq_pt :
+            eq_pt = float(stock[i][0])
+            pos_eq = i
 
     print("Equivalence point at %.1f ml" % eq_pt)
     return (pos_eq)
